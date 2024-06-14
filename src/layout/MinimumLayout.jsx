56b56d27ae5c55b0774cb54ee4b80413134bfe9b
subekshathing <shuvechchhathing@gmail.com>
2024-06-14 14:31:25 +0545

    updated

--- src/layout/MinimumLayout.jsx
+++ src/layout/MinimumLayout.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../component/Header";
 import CustomSnackbar from "../component/CustomerSnackbar";
+import Home from "../pages/HomePage/Home";
 
 const MinimumLayout = () => {
   return (
     <>
       <Header />
-
+      <Home />
       <CustomSnackbar />
       <Outlet />
     </>
